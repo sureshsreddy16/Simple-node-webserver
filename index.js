65a22160a7f6af776a386b161d5480a2d53622f9
sureshsreddy16 <57609555+sureshsreddy16@users.noreply.github.com>
2019-11-28 09:25:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
+  res.write('Hello India!'); //write a response to the client
   res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
+}).listen(4000); //the server object listens on port 8080
 
-console.log ("Point your broswer to Port http://localhost:3000");
+console.log ("Point your broswer to Port http://localhost:4000");
 
